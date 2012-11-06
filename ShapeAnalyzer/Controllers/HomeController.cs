2426cdc6d5e6ebebd05f04ff964adaf6d3e34cd7
Daniel Pratt <daniel.pratt@outlook.com>
2012-11-05 21:51:04 -0500

    added manual test; formatting tweaks

--- ShapeAnalyzer/Controllers/HomeController.cs
+++ ShapeAnalyzer/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ShapeAnalyzer.App_Start;
+using ShapeAnalyzer.Models;
 
 namespace ShapeAnalyzer.Controllers
 {
@@ -14,69 +15,93 @@ namespace ShapeAnalyzer.Controllers
 
         public ActionResult Index()
         {
-            var r = new Random();
+            return View();
+        }
+
+        public ViewResult TestCases()
+        {
+            var model = new ShapeAnalyzerTests
+            {
+                RandomCircles = GetRandomCircleTestCases(),
+                PathalogicalCircles = GetPathologicalCircleTestCases(),
+                BrokenCircles = GetBrokenCircleTestCases(),
+            };
+
+            return View(model);            
+        }
+
+        public ViewResult ManualTest(string imageUrl)
+        {
+            ViewBag.ImageUrl = imageUrl;
+
+            return View();
+        }
+
+        private IEnumerable<ShapeAnalyzerTestCircle> GetBrokenCircleTestCases()
+        {
+            var random = new Random();
+
+            var brokenCircles =
+                from c in Enumerable.Range(0, 10)
+                let randomX = random.Next(1, 50)
+                let randomY = random.Next(1, 50)
+                let wiggleX = random.Next(3, 10)
+                let wiggleY = random.Next(3, 10)
+                select new Models.ShapeAnalyzerTestCircle
+                           {
+                               CenterX = randomX - wiggleX,
+                               CenterY = randomY - wiggleY,
+                               Url = Url.Action("RandomizedCircleImage", new {centerX = randomX, centerY = randomY}),
+                           };
+            return brokenCircles;
+        }
+
+        private IEnumerable<ShapeAnalyzerTestCircle> GetRandomCircleTestCases()
+        {
+            var random = new Random();
 
             var randomCircles =
                 from c in Enumerable.Range(0, 10)
-                let randomX = r.Next(1, 50)
-                let randomY = r.Next(1, 50)
+                let randomX = random.Next(1, 50)
+                let randomY = random.Next(1, 50)
                 select new Models.ShapeAnalyzerTestCircle
                            {
                                CenterX = randomX,
                                CenterY = randomY,
-                               Url = Url.Action("RandomizedCircleImage", new { centerX = randomX, centerY = randomY }),
+                               Url = Url.Action("RandomizedCircleImage", new {centerX = randomX, centerY = randomY}),
                            };
+            return randomCircles;
+        }
 
-            var badCircles = new[]
-                                 {
-                                     new Models.ShapeAnalyzerTestCircle
-                                         {
-                                             CenterX = 1,
-                                             CenterY = 1,
-                                             Url = Url.Content("~/Content/images/bad-circle-1.png"),
-                                         },
-                                     new Models.ShapeAnalyzerTestCircle
-                                         {
-                                             CenterX = 99,
-                                             CenterY = 1,
-                                             Url = Url.Content("~/Content/images/bad-circle-2.png"),
-                                         },
-                                     new Models.ShapeAnalyzerTestCircle
-                                         {
-                                             CenterX = 1,
-                                             CenterY = 99,
-                                             Url = Url.Content("~/Content/images/bad-circle-3.png"),
-                                         },
-                                     new Models.ShapeAnalyzerTestCircle
-                                         {
-                                             CenterX = 99,
-                                             CenterY = 99,
-                                             Url = Url.Content("~/Content/images/bad-circle-4.png"),
-                                         },
-                                 };
-
-            var brokenCircles =
-                from c in Enumerable.Range(0, 10)
-                let randomX = r.Next(1, 50)
-                let randomY = r.Next(1, 50)
-                let wiggleX = r.Next(3, 10)
-                let wiggleY = r.Next(3, 10)
-                select new Models.ShapeAnalyzerTestCircle
-                {
-                    CenterX = randomX - wiggleX,
-                    CenterY = randomY - wiggleY,
-                    Url = Url.Action("RandomizedCircleImage", new { centerX = randomX, centerY = randomY }),
-                };
-
-
-            var model = new Models.ShapeAnalyzerTests
-                            {
-                                RandomCircles = randomCircles,
-                                PathalogicalCircles = badCircles,
-                                BrokenCircles = brokenCircles,
-                            };
-
-            return View(model);
+        private IEnumerable<ShapeAnalyzerTestCircle> GetPathologicalCircleTestCases()
+        {
+            return new[]
+                       {
+                           new Models.ShapeAnalyzerTestCircle
+                               {
+                                   CenterX = 1,
+                                   CenterY = 1,
+                                   Url = Url.Content("~/Content/images/bad-circle-1.png"),
+                               },
+                           new Models.ShapeAnalyzerTestCircle
+                               {
+                                   CenterX = 99,
+                                   CenterY = 1,
+                                   Url = Url.Content("~/Content/images/bad-circle-2.png"),
+                               },
+                           new Models.ShapeAnalyzerTestCircle
+                               {
+                                   CenterX = 1,
+                                   CenterY = 99,
+                                   Url = Url.Content("~/Content/images/bad-circle-3.png"),
+                               },
+                           new Models.ShapeAnalyzerTestCircle
+                               {
+                                   CenterX = 99,
+                                   CenterY = 99,
+                                   Url = Url.Content("~/Content/images/bad-circle-4.png"),
+                               },
+                       };
         }
 
         public FileStreamResult RandomizedCircleImage(int centerX, int centerY, int? width)
